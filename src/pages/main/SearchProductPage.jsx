8777daae0901d636617889ac60c309701ce5f0f0
pvdkhoa <phamvudangkhoa2002@gmail.com>
2023-12-14 16:41:43 +0700

    Complete SearchPage

---
+++ src/pages/main/SearchProductPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import SearchProduct from "../../components/search/SearchProduct";
+import { useNavigate } from "react-router-dom";
+
+
+const SearchProductPage = () => {
+  const navigate = useNavigate();
+  const handleBackButtonClick = () => {
+    navigate("/home");
+  };
+  return (
+    <div>
+      <div className="w-20 h-10 bg-[#52AB98] mt-16 mb-12 ml-4 flex items-center justify-center rounded-lg font-semibold text-white ">
+        <button onClick={handleBackButtonClick}>Back</button>
+      </div>
+      <SearchProduct></SearchProduct>
+    </div>
+  );
+};
+
+export default SearchProductPage;
